tiempolocoge/dracut-ng

commit d4071bf5401f4166c67805aa94c87d7714a08b8a

i18n/parse-i18n.sh: corrected the right option to use for the cmdline

modules.d/10i18n/parse-i18n.sh
@@ -19,12 +19,12 @@ inst_key_val() {
     unset _value
 }
 
-inst_key_val '' /etc/vconsole.conf KEYMAP      vconsole.keymap      -d KEYTABLE
-inst_key_val '' /etc/vconsole.conf FONT        vconsole.font        -d SYSFONT
-inst_key_val '' /etc/vconsole.conf FONT_MAP    vconsole.font.map    -d CONTRANS
-inst_key_val '' /etc/vconsole.conf FONT_UNIMAP vconsole.font.unimap -d UNIMAP
-inst_key_val 1  /etc/vconsole.conf UNICODE     vconsole.unicode vconsole.font.unicode
-inst_key_val '' /etc/vconsole.conf EXT_KEYMAP  vconsole.keymap.ext
+inst_key_val '' /etc/vconsole.conf vconsole.keymap      KEYMAP      -d KEYTABLE
+inst_key_val '' /etc/vconsole.conf vconsole.font        FONT        -d SYSFONT
+inst_key_val '' /etc/vconsole.conf vconsole.font.map    FONT_MAP    -d CONTRANS
+inst_key_val '' /etc/vconsole.conf vconsole.font.unimap FONT_UNIMAP -d UNIMAP
+inst_key_val 1  /etc/vconsole.conf vconsole.font.unicode UNICODE vconsole.unicode
+inst_key_val '' /etc/vconsole.conf vconsole.keymap.ext  EXT_KEYMAP
 
 inst_key_val '' /etc/locale.conf   LANG        locale.LANG
 inst_key_val '' /etc/locale.conf   LC_ALL      locale.LC_ALL

commit 5df53b8adb7752cb0232d55b1eee716b595c4c40

i18n/parse-i18n.sh: fix locale.conf caused by d4071bf5401f4166c678
https://bugzilla.redhat.com/show_bug.cgi?id=870632

modules.d/10i18n/parse-i18n.sh
@@ -26,8 +26,8 @@ inst_key_val '' /etc/vconsole.conf vconsole.font.unimap FONT_UNIMAP -d UNIMAP
 inst_key_val 1  /etc/vconsole.conf vconsole.font.unicode UNICODE vconsole.unicode
 inst_key_val '' /etc/vconsole.conf vconsole.keymap.ext  EXT_KEYMAP
 
-inst_key_val '' /etc/locale.conf   LANG        locale.LANG
-inst_key_val '' /etc/locale.conf   LC_ALL      locale.LC_ALL
+inst_key_val '' /etc/locale.conf   locale.LANG   LANG
+inst_key_val '' /etc/locale.conf   locale.LC_ALL LC_ALL
 
 if [ -f /etc/locale.conf ]; then
     . /etc/locale.conf