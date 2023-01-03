tiempolocoge/dracut-ng

commit 8975b8232a5625385544bac37f180701f62d9181

dmsquash: add nasty hack for cdroms
Some CDROMs do not generate a label symlink and have to be read from.

modules.d/90dmsquash-live/parse-dmsquash-live.sh
@@ -30,11 +30,12 @@ case "$liveroot" in
 	root="${root#live:}"
 	root="live:/dev/disk/by-uuid/${root#UUID=}"
         rootok=1 ;;
-    /dev/*)
-	root="live:${root}"
+    live:/dev/*)
         rootok=1 ;;
 esac
 info "root was $root, liveroot is now $liveroot"
+
+[ $rootok = "1" ] && initqueue --settled /sbin/cdrom-hack.sh
 
 # make sure that init doesn't complain
 [ -z "$root" ] && root="live"

modules.d/90dmsquash-live/cdrom-hack.sh
@@ -1,0 +1,6 @@
+#!/bin/sh
+
+[ -e /dev/sr0 ] && {
+	blkid /dev/sr0;
+	blkid /dev/sr0;
+} 2>/dev/null

modules.d/90dmsquash-live/install
@@ -20,3 +20,4 @@ inst_hook pre-udev 30 "$moddir/dmsquash-live-genrules.sh"
 inst "$moddir/dmsquash-live-root" "/sbin/dmsquash-live-root"
 # should probably just be generally included
 inst_rules 60-cdrom_id.rules
+inst "$moddir/cdrom-hack.sh" /sbin/cdrom-hack.sh

commit a380425cb1af697bf1a3827d807d2599fa0f023a

fix(dmsquash-live): live:/dev/*
fix a bug introduced in 8975b8232

make sure root is set correctly for both of the following cases
 - "root=live:/dev/*"
 - "rd.live.image root=/dev/*"

modules.d/90dmsquash-live/parse-dmsquash-live.sh
@@ -34,6 +34,7 @@ case "$liveroot" in
         rootok=1
         ;;
     live:/dev/*)
+        root="live:${root#live:}"
         rootok=1
         ;;
     live:/*.[Ii][Mm][Gg] | /*.[Ii][Mm][Gg])