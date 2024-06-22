tiempolocoge/dracut-ng

commit 0e5971e63698a4ba036f6e1453d7a8eee965be46

refactor(convertfs): drop duplicate def. of ismounted()

modules.d/30convertfs/convertfs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+type ismounted > /dev/null 2>&1 || . /lib/dracut-lib.sh
+
 ROOT="$1"
 
 if [[ ! -d $ROOT ]]; then
@@ -78,26 +80,6 @@ find_mount() {
     done < /proc/mounts
     return 1
 }
-
-# usage: ismounted <mountpoint>
-# usage: ismounted /dev/<device>
-if command -v findmnt > /dev/null; then
-    ismounted() {
-        findmnt "$1" > /dev/null 2>&1
-    }
-else
-    ismounted() {
-        if [ -b "$1" ]; then
-            find_mount "$1" > /dev/null && return 0
-            return 1
-        fi
-
-        while read -r _ m _ || [ -n "$m" ]; do
-            [ "$m" = "$1" ] && return 0
-        done < /proc/mounts
-        return 1
-    }
-fi
 
 # clean up after ourselves no matter how we die.
 cleanup() {

modules.d/30convertfs/module-setup.sh
@@ -8,8 +8,7 @@ check() {
 
 # called by dracut
 depends() {
-    echo bash
-    return 0
+    echo base bash
 }
 
 # called by dracut

commit 237fdc951b4261975d0fca845161b4759da646af

fix(convertfs): drop unused find_mount function
shellcheck complains about SC2317 (info): Command appears to be
unreachable. Check usage (or ignore if invoked indirectly).

The function `find_mount` is not called in `convertfs.sh` any more. So
drop this unused function.

Fixes: 0e5971e6369 ("refactor(convertfs): drop duplicate def. of ismounted()")
Signed-off-by: Benjamin Drung <benjamin.drung@canonical.com>

modules.d/30convertfs/convertfs.sh
@@ -72,15 +72,6 @@ if [[ ! -e $testfile ]]; then
 fi
 rm -f -- "$testfile"
 
-find_mount() {
-    local dev wanted_dev
-    wanted_dev="$(readlink -e -q "$1")"
-    while read -r dev _ || [ -n "$dev" ]; do
-        [ "$dev" = "$wanted_dev" ] && echo "$dev" && return 0
-    done < /proc/mounts
-    return 1
-}
-
 # clean up after ourselves no matter how we die.
 # shellcheck disable=SC2317  # called via EXIT trap
 cleanup() {